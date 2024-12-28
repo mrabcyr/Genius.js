d93417e6b34f74ae5c8759b15fbaae4cf75c2728
mrabcyr <katerinarycbarm@gmail.com>
2024-12-29 01:00:05 +0200

    new

---
+++ Homework-js/Homework-three.js
@@ -0,0 +1,20 @@
+////---- ОБЛАСТІ ВИДИМОСТІ ----////
+
+/* const a = 5;
+
+if (true) {
+    const b = 10;
+    console.log("Block", b);
+}
+
+console.log("Global", a); */
+
+
+////---- ЦИКЛИ ----////
+
+let a = 0;
+
+while (a < 5) {
+    a++;
+    console.log("a", a);
+}
