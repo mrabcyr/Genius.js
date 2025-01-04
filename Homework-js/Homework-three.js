58273400a61fbe07a421135d001955adf16c9742
mrabcyr <katerinarycbarm@gmail.com>
2025-01-04 21:09:13 +0200

    new

--- Homework-js/Homework-three.js
+++ Homework-js/Homework-three.js
@@ -12,9 +12,74 @@ console.log("Global", a); */
 
 ////---- ЦИКЛИ ----////
 
-let a = 0;
+//--WHILE--//
+
+/* let a = 0;
 
 while (a < 5) {
-    a++;
+    a++;   
     console.log("a", a);
 }
+
+// a++ те саме, що a+1;   a-- те саме, що а-1 */
+
+//let a = 0;
+
+/* while (a) {
+    console.log("a", a);
+    a--;
+} */
+
+// АБО //
+
+//while(a) console.log(a--);
+
+
+
+//--DO WHILE--//
+
+/*do {
+    console.log(a++);
+} while (a<5);*/
+
+
+
+//--FOR--//
+
+/*for (let a = 0; a < 3; a++) {
+    console.log(a);
+}*/
+
+// АБО //
+
+/*let a = 0
+for (; a < 5; a++) {
+    console.log(a);
+}*/
+
+
+
+//--BREAK & CONTINUE--//
+
+/*let sum = 0;
+
+while (true) {
+    let value = +prompt("Введіть число")
+    if (!value) break
+    sum += value
+}
+
+console.log("Сума", sum);*/
+
+
+/*for (let i = 0; i < 10; i++) {
+    if (i % 2 === 0) continue;
+    console.log(i);
+}*/
+
+
+
+
+
+
+////////-------- МАСИВИ --------////////
